eb521b980fe810c949ed22aa3b4b079e479948d2
srodif <91968331+srodif@users.noreply.github.com>
2022-01-02 18:49:52 +0200

    Add files via upload

--- moveCompare.py
+++ moveCompare.py
@@ -1,28 +1,34 @@
 print("Hello")
-playerM = "a"
-computerM = "b"
+playerIn = "a"
+computerIn = "c"
 result = 1
 print(result)
-if playerM == "a" :
-    if computerM == "a" : 
-        result = 1
-    elif computerM == "b" :
-        result = 2
-    else :
-        result = 3
-elif playerM == "b" :
-    if computerM == "b" :
-        result = 1
-    elif computerM == "c" :
-        result = 2
-    else :
-        result = 3
-else :
-    if computerM == "c" :
-        result = 1
-    elif computerM == "a" :
-        result = 2
-    else :
-        result = 3
 
+def movecompare(playerM,computerM) :
+    '''asd
+    asd'''
+    print("funRun")
+    if playerM == "a" :
+     if computerM == "a" : 
+        result = 1
+     elif computerM == "b" :
+        result = 2
+     else :
+        result = 3
+    elif playerM == "b" :
+        if computerM == "b" :
+            result = 1
+        elif computerM == "c" :
+            result = 2
+        else :
+            result = 3
+    else :
+      if computerM == "c" :
+        result = 1
+      elif computerM == "a" :
+        result = 2
+      else :
+        result = 3
+    return(result)
+result = movecompare(playerIn,computerIn)
 print(result)
